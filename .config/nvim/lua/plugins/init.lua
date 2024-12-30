ccfb5975987cf474c251a230ac3cf57bc71d8c58
raxl8 <32034076+raxl8@users.noreply.github.com>
2024-12-30 23:11:13 +0000

    refactor(.config/nvim): remove useless comments

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,8 +4,6 @@ return {
     event = "BufWritePre",
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
