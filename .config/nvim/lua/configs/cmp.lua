e73e6fdcea321a3752dc6b4b83bceeccebfee663
raxl8 <32034076+raxl8@users.noreply.github.com>
2024-12-31 17:00:47 +0000

    refactor(.config/nvim): ctrl+y to accept cmp suggestion
    tmux doesn't send ctrl+enter
    didn't bother trying to fix it so switching to ctrl+y

--- .config/nvim/lua/configs/cmp.lua
+++ .config/nvim/lua/configs/cmp.lua
@@ -1,6 +1,6 @@
 return function(_, opts)
   local cmp = require "cmp"
-  opts.mapping["<C-CR>"] = opts.mapping["<CR>"]
+  opts.mapping["<C-y>"] = opts.mapping["<CR>"]
   opts.mapping["<C-n>"] = opts.mapping["<Tab>"]
   opts.mapping["<C-p>"] = opts.mapping["<S-Tab>"]
   opts.mapping["<CR>"] = nil
