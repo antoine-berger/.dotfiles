344c19a3cebf90270e33f28017ff8481a7300264
raxl8 <32034076+raxl8@users.noreply.github.com>
2024-12-31 16:58:56 +0000

    feat(.config/nvim): don't link system clipboard anymore

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,6 +1,14 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del;
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- clipboard
+unmap("n", "<leader>pt")
+map("n", "<leader>y", "\"+y", { desc = "Yank to system clipboard" })
+map("v", "<leader>y", "\"+y", { desc = "Yank to system clipboard" })
+map("n", "<leader>p", "\"+p", { desc = "Paste from system clipboard" })
+map("v", "<leader>p", "\"+p", { desc = "Paste from system clipboard" })
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,9 +1,7 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.clipboard = "";
 
 local function is_wsl()
   local version_file = io.open("/proc/version", "rb")
