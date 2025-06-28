28d9cafed1aa477dd983c5a6b48e1e84dd5e0d94
raxl8 <32034076+raxl8@users.noreply.github.com>
2025-06-28 12:26:02 +0200

    feat(nix/homebrew): update package list

--- nix/configs/darwin/homebrew.nix
+++ nix/configs/darwin/homebrew.nix
@@ -8,32 +8,40 @@
       autoUpdate = true;
       upgrade = true;
     };
+    brews = [
+      "weasyprint"
+    ];
     casks = [
       "alcove"
+      "android-studio"
       "appcleaner"
       "clop"
       "discord"
-      "docker"
-      "flux"
+      "docker-desktop"
       "firefox"
+      "flux"
       "ghostty"
       "ibkr"
+      "insomnia"
       "karabiner-elements"
       "latest"
       "lookaway"
       "middleclick"
+      "notion"
       "proton-drive"
       "proton-mail-bridge"
       "proton-pass"
       "protonvpn"
+      "qbittorrent"
       "raycast"
+      "rustdesk"
       "slack"
       "screen-studio"
       "shottr"
       "spotify"
       "tradingview"
       "vanilla"
-      "zen-browser"
+      "vlc"
       "zoom"
     ];
     masApps = {
