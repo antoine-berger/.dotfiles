76a41d72748a54fd16e98d79d84bc8584db7346e
raxl8 <32034076+raxl8@users.noreply.github.com>
2025-06-28 12:27:02 +0200

    feat(nix/home): enable xdg

--- nix/configs/darwin/home.nix
+++ nix/configs/darwin/home.nix
@@ -1,4 +1,6 @@
 {lib, ...}: {
+  xdg.enable = true;
+
   home.activation = {
     link-nushell-config = lib.hm.dag.entryAfter ["writeBoundary"] ''
       run ln -sf $HOME/.config/nushell $HOME/Library/Application\ Support/nushell
